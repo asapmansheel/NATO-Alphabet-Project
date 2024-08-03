60fe2a5e64b5331f594d34f2637f8e3e61fa3896
asapmansheel <70127478+asapmansheel@users.noreply.github.com>
2024-08-02 20:45:40 -0400

    Day 30 of 100 Days of Python
    Updated code to include a try and catch block

--- main.py
+++ main.py
@@ -7,7 +7,15 @@ data = pd.read_csv('/Users/macintosh/Downloads/NATO-alphabet-start/nato_phonetic
 nato_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input('Enter a word: ').upper()
+def generate_phonetic():
+    word = input('Enter a word: ').upper()
+    
+    try:
+        list = [nato_dict[letter] for letter in word]
+    except KeyError:
+        print('Sorry, only letters in the alphabet please.')
+        generate_phonetic()
+    else:
+        print(list)
 
-list = [nato_dict[letter] for letter in word]
-print(list)
+generate_phonetic()
